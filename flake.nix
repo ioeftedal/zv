e268914f8a1702e8798b3072f62e625a2baf14a2
fatum <codeberg.primer254@passmail.net>
2026-07-04 18:05:30 +0200

    init: nix flake init

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Zig project template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls-overlay.url = "github:zigtools/zls";
+    zigdoc-nix.url = "github:uzaaft/zigdoc-nix";
+    ziglint-nix.url = "github:uzaaft/ziglint-nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    zig-overlay,
+    zls-overlay,
+    zigdoc-nix,
+    ziglint-nix,
+    ...
+  }: let
+    allSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
+        });
+  in {
+    packages = forAllSystems ({
+      pkgs,
+      system,
+    }: let
+      zig = zig-overlay.packages.${system}.master;
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "zig-project";
+        src = ./.;
+        nativeBuildInputs = [zig];
+
+        buildPhase = ''
+          export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+          export ZIG_LOCAL_CACHE_DIR=$TMPDIR/zig-cache
+          zig build -Doptimize=ReleaseSafe --prefix $out
+        '';
+
+        dontInstall = true;
+      };
+    });
+
+    devShells = forAllSystems ({
+      pkgs,
+      system,
+    }: let
+      zig = zig-overlay.packages.${system}.master;
+      zls = zls-overlay.packages.${system}.zls;
+      zigdoc = zigdoc-nix.packages.${system}.default;
+      ziglint = ziglint-nix.packages.${system}.default;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [
+          zig
+          zls
+          zigdoc
+          ziglint
+        ];
+      };
+    });
+  };
+}
